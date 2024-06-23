f11b3d57baa016e6484c5be6c59697a9934d82a8
crsvitor <dev.castrors@gmail.com>
2024-06-23 20:33:07 -0300

    feat: add game class
    also, correct minor details over characters classes

--- game.py
+++ game.py
@@ -33,7 +33,7 @@ class Character:
         damage = random.randint(min_damage, max_damage)
         target.get_attacked(damage)
 
-        message = f"{self.get_name} attacked {target.get_name}, taking {damage} damage points!"
+        message = f"{self.get_name()} attacked {target.get_name()}, taking {damage} damage points!"
         print(message)
 
 class Hero(Character):
@@ -45,7 +45,7 @@ class Hero(Character):
         return self.__power
     
     def get_details(self):
-        return f"{super().get_details()} \nPower: {self.get_power()}"
+        return f"{super().get_details()} \nPower: {self.get_power()}\n"
     
     def special_attack(self, target):
         level = self.get_level()
@@ -56,7 +56,7 @@ class Hero(Character):
         damage = random.randint(min_damage, max_damage)
         target.get_attacked(damage)
 
-        message = f"{self.get_name} special attacked {target.get_name}, taking {damage} damage points!"
+        message = f"{self.get_name()} special attacked {target.get_name()}, taking {damage} damage points!"
         print(message)
 
 
@@ -69,5 +69,36 @@ class Enemy(Character):
         return self.__type
     
     def get_details(self):
-        return f"{super().get_details()} \nType: {self.get_type()}"
+        return f"{super().get_details()} \nType: {self.get_type()}\n"
     
+class Game:
+    def __init__(self) -> None:
+        self.__hero = Hero(name="Hero", life=100, level=5, power="Super strength")
+        self.__enemy = Enemy(name="Bat", life=50, level=5, type="Flying")
+
+    def start_battle(self):
+        while self.__hero.get_life() > 0 and self.__enemy.get_life() > 0:
+            print("\nCharacters:")
+            print(self.__hero.get_details())
+            print(self.__enemy.get_details())
+
+            input("Press enter to attack... ")
+            choice = int(input("Choose (1 - normal attack, 2 - special attack): "))
+
+            if choice == 1:
+                self.__hero.attack(self.__enemy)
+            elif choice == 2:
+                self.__hero.special_attack(self.__enemy)
+            else:
+                print("Invalid choice, select again.")
+
+            if self.__hero.get_life() > 0:
+                self.__enemy.attack(self.__hero)
+            
+        if self.__hero.get_life() > 0:
+            print("\nCongratulations! You won the battle!")
+        else:
+            print("\nYou lost the battle!")
+
+game = Game()
+game.start_battle()
